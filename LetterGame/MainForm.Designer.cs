391192608df6d6d3ae3b6c56b720d6a0e0ccff08
erenright <erenright@protonmail.ch>
2021-05-28 00:53:24 -0400

    Initial commit of program code

---
+++ LetterGame/MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace LetterGame
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLetter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblLetter
+            // 
+            this.lblLetter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblLetter.Font = new System.Drawing.Font("Microsoft Sans Serif", 200F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLetter.Location = new System.Drawing.Point(0, 0);
+            this.lblLetter.Name = "lblLetter";
+            this.lblLetter.Size = new System.Drawing.Size(621, 441);
+            this.lblLetter.TabIndex = 0;
+            this.lblLetter.Text = "lblLetter";
+            this.lblLetter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 441);
+            this.Controls.Add(this.lblLetter);
+            this.Name = "frmMain";
+            this.Text = "Letter Game";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.frmMain_KeyPress);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLetter;
+    }
+}
+
---
+++ LetterGame/MainForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LetterGame
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+
+            SetRandomLetter();
+        }
+
+        /// <summary>
+        /// Updates the letter label with a random character between 'A' and 'Z', inclusive.
+        /// If the letter would be the same, a different one is randomly selected to ensure
+        /// that it is different every time.
+        /// </summary>
+        private void SetRandomLetter()
+        {
+            var rand = new Random();
+            string s = lblLetter.Text;
+
+            // Prevent the same char from showing up twice in a row
+            while (s == lblLetter.Text)
+            {
+                lblLetter.Text = ((char)rand.Next((int)'A', (int)'Z' + 1)).ToString();
+            }
+        }
+
+        /// <summary>
+        /// Event handler for form keystrokes.  If the pressed key matches the displayed
+        /// letter, pick a new random letter.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmMain_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.ToString().ToUpper() == lblLetter.Text)
+            {
+                SetRandomLetter();
+            }
+        }
+    }
+}
